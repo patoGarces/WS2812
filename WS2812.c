f47f1a34e862cfd1fbdbfa413e695b512c928ea1
PatricioGarces <patricio.garces@outlook.com>
2024-03-03 21:29:52 -0300

    Fix error en el CMakeList, agrego status sequences

--- WS2812.c
+++ WS2812.c
@@ -1,38 +1,48 @@
 #include <stdio.h>
 #include "include/WS2812.h"
 #include "driver/i2s.h"
-#include "include/ws2812.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-#define TAG "ws2812"
+#define TAG "WS2812"
+
+const pixel_color_t colors[8] = {
+    {.red = 0x00,.green = 0x00,.blue = 0x00},
+    {.red = 0xff,.green = 0x00,.blue = 0x00},
+    {.red = 0x00,.green = 0xff,.blue = 0x00},
+    {.red = 0x00,.green = 0x00,.blue = 0xff},
+    {.red = 0xff,.green = 0x00,.blue = 0xff},
+    {.red = 0xff,.green = 0xff,.blue = 0x00},
+    {.red = 0x00,.green = 0xff,.blue = 0xff},
+    {.red = 0xff,.green = 0xff,.blue = 0xff}
+};
 
 static uint8_t out_buffer[CANT_LEDS * 12] = {0};
 static uint8_t off_buffer[ZERO_BUFFER] = {0};
 static uint16_t size_buffer;
-
 static const uint16_t bitpatterns[4] = {0x88, 0x8e, 0xe8, 0xee};
 
+TaskHandle_t xControlStatusLed;
+status_pixel_control_t statusLedControl;
 
-void updatePixel(pixel_color_t *pixels) {
+static void statusLedHandler(void *pvParameters);
+
+void updatePixel(pixel_color_t pixel) {
   size_t bytes_written = 0;
+  out_buffer[0] = bitpatterns[pixel.green >> 6 & 0x03];
+  out_buffer[1] = bitpatterns[pixel.green >> 4 & 0x03];
+  out_buffer[2] = bitpatterns[pixel.green >> 2 & 0x03];
+  out_buffer[3] = bitpatterns[pixel.green & 0x03];
 
-  for (uint16_t i = 0; i < CANT_LEDS; i++) {
-    int loc = i * 12;
+  out_buffer[4] = bitpatterns[pixel.red >> 6 & 0x03];
+  out_buffer[5] = bitpatterns[pixel.red >> 4 & 0x03];
+  out_buffer[6] = bitpatterns[pixel.red >> 2 & 0x03];
+  out_buffer[7] = bitpatterns[pixel.red & 0x03];
 
-    out_buffer[loc] = bitpatterns[pixels[i].green >> 6 & 0x03];
-    out_buffer[loc + 1] = bitpatterns[pixels[i].green >> 4 & 0x03];
-    out_buffer[loc + 2] = bitpatterns[pixels[i].green >> 2 & 0x03];
-    out_buffer[loc + 3] = bitpatterns[pixels[i].green & 0x03];
-
-    out_buffer[loc + 4] = bitpatterns[pixels[i].red >> 6 & 0x03];
-    out_buffer[loc + 5] = bitpatterns[pixels[i].red >> 4 & 0x03];
-    out_buffer[loc + 6] = bitpatterns[pixels[i].red >> 2 & 0x03];
-    out_buffer[loc + 7] = bitpatterns[pixels[i].red & 0x03];
-
-    out_buffer[loc + 8] = bitpatterns[pixels[i].blue >> 6 & 0x03];
-    out_buffer[loc + 9] = bitpatterns[pixels[i].blue >> 4 & 0x03];
-    out_buffer[loc + 10] = bitpatterns[pixels[i].blue >> 2 & 0x03];
-    out_buffer[loc + 11] = bitpatterns[pixels[i].blue & 0x03];
-  }
+  out_buffer[8] = bitpatterns[pixel.blue >> 6 & 0x03];
+  out_buffer[9] = bitpatterns[pixel.blue >> 4 & 0x03];
+  out_buffer[10] = bitpatterns[pixel.blue >> 2 & 0x03];
+  out_buffer[11] = bitpatterns[pixel.blue & 0x03];
 
   i2s_write(PERIPH_I2S_NUM, out_buffer, size_buffer, &bytes_written, portMAX_DELAY);
   i2s_write(PERIPH_I2S_NUM, off_buffer, ZERO_BUFFER, &bytes_written, portMAX_DELAY);
@@ -40,7 +50,7 @@ void updatePixel(pixel_color_t *pixels) {
   i2s_zero_dma_buffer(PERIPH_I2S_NUM);
 }
 
-void sw2812Init(uint8_t _gpioPin) {
+void statusLedInit(uint8_t _gpioPin) {
     i2s_config_t i2s_config = {
         .mode = I2S_MODE_MASTER | I2S_MODE_TX,
         .sample_rate = SAMPLE_RATE,
@@ -59,8 +69,77 @@ void sw2812Init(uint8_t _gpioPin) {
         .data_in_num = -1
     };
 
-    size_buffer = CANT_LEDS * 12;
-    i2s_config.dma_buf_len = size_buffer;
+    size_buffer = 12;
+    i2s_config.dma_frame_num = size_buffer;   
     i2s_driver_install(PERIPH_I2S_NUM, &i2s_config, 0, NULL);
     i2s_set_pin(PERIPH_I2S_NUM, &pin_config);
+
+    statusLedUpdate(STATUS_LED_INITIALIZING);
+    xTaskCreate(statusLedHandler,"status led task",4096,NULL,3,&xControlStatusLed);
+}
+
+void statusLedUpdate(uint8_t status) {
+
+  switch(status) {
+    case STATUS_LED_INITIALIZING:
+      statusLedControl.blink = false;
+      statusLedControl.colors[0] = colors[COLOR_WHITE];
+      statusLedControl.tOnMs = 500;
+    break;
+    case STATUS_LED_ERROR:
+      statusLedControl.blink = false;
+      statusLedControl.colors[0] = colors[COLOR_RED];
+      statusLedControl.tOnMs = 500;
+    break;
+
+    case STATUS_LED_WAITING_ARM:
+      statusLedControl.blink = true;
+      statusLedControl.colors[0] = colors[COLOR_YELLOW];
+      statusLedControl.colors[1] = colors[COLOR_GREEN];
+      statusLedControl.tOnMs = 500;
+      statusLedControl.tOffMs = 500;
+    break;
+
+    case STATUS_LED_ARMED:
+      statusLedControl.blink = false;
+      statusLedControl.colors[0] = colors[COLOR_GREEN];
+      statusLedControl.tOnMs = 500;
+      statusLedControl.tOffMs = 500;
+    break;
+
+    case STATUS_LED_FAILSAFE:
+      statusLedControl.blink = true;
+      statusLedControl.colors[0] = colors[COLOR_PURPLE];
+      statusLedControl.colors[1] = colors[COLOR_BLACK];
+      statusLedControl.tOnMs = 500;
+      statusLedControl.tOffMs = 200;
+    break;
+
+    case STATUS_LED_CALIBRATION_IMU:
+      statusLedControl.blink = true;
+      statusLedControl.colors[0] = colors[COLOR_BLUE];
+      statusLedControl.colors[1] = colors[COLOR_BLACK];
+      statusLedControl.tOnMs = 50;
+      statusLedControl.tOffMs = 200;
+    break;
+
+    // default:
+    //   statusLedControl.blink = false;
+    //   statusLedControl.colors[0] = colors[COLOR_BLACK];
+    //   statusLedControl.tOnMs = 500;
+    // break;
+  }
+}
+
+static void statusLedHandler(void *pvParameters) {
+  while(true){
+    updatePixel(colors[COLOR_BLUE]);//updatePixel(statusLedControl.colors[0]);
+    vTaskDelay(pdMS_TO_TICKS(100));//pdMS_TO_TICKS(statusLedControl.tOnMs));
+    // if (statusLedControl.blink) {
+      updatePixel(colors[COLOR_BLACK]);//updatePixel(statusLedControl.colors[1]);
+      vTaskDelay(pdMS_TO_TICKS(100));//vTaskDelay(pdMS_TO_TICKS(statusLedControl.tOffMs));
+    // }
+  }
+
+  vTaskDelete(NULL);
 }
--- include/WS2812.h
+++ include/WS2812.h
@@ -12,8 +12,22 @@ void func(void);
 typedef enum {
     STATUS_LED_INITIALIZING,
     STATUS_LED_ERROR,
+    STATUS_LED_WAITING_ARM,
+    STATUS_LED_ARMED,
     STATUS_LED_FAILSAFE,
+    STATUS_LED_CALIBRATION_IMU
 } StatusLed;
+
+typedef enum {
+    COLOR_BLACK,
+    COLOR_RED,
+    COLOR_GREEN,
+    COLOR_BLUE,
+    COLOR_PURPLE,
+    COLOR_YELLOW,
+    COLOR_SKY_BLUE,
+    COLOR_WHITE
+} LedColors;
 
 typedef struct {
     uint8_t red;
@@ -21,8 +35,14 @@ typedef struct {
     uint8_t blue;
 } pixel_color_t;
 
-void sw2812Init(uint8_t gpioPin);
-void updateStatus(uint8_t status);
-void updatePixel(pixel_color_t *pixels); // TODO: borrar
+typedef struct {
+    uint8_t blink;
+    uint16_t tOnMs;
+    uint16_t tOffMs;
+    pixel_color_t colors[2];
+} status_pixel_control_t;
+
+void statusLedInit(uint8_t gpioPin);
+void statusLedUpdate(uint8_t status);
 
 #endif
